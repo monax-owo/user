107fb96ebda6fe7310a5dc034a0368ae969be198
zprl <zprl.dev@gmail.com>
2024-06-17 08:59:01 +0900

    up

--- userscript/boom/dist/boom.user.js
+++ userscript/boom/dist/boom.user.js
@@ -372,7 +372,7 @@
     console.log("working!");
     let nth = 1;
     const deletePost = () => {
-      const MAX = 100;
+      const MAX = 1e3;
       let loop = 0;
       const feedList = document.getElementsByClassName("feedListLayout_feed_list__Z1Jh_")[0];
       while (loop < MAX) {
@@ -382,32 +382,32 @@
         if (postId === void 0 || postId === "") break;
         if (post.querySelector(".media_image") === null) {
           console.log("no image");
-          fetch("https://linevoom.line.me/api/post/delete?homeId=_dVkHnuS19n3F4QZRu6woElyJ1YUwrYkVVDKc2mk", {
-            headers: {
-              accept: "application/json, text/plain, */*",
-              "accept-language": "ja,en-US;q=0.9,en;q=0.8",
-              baggage: "sentry-environment=production,sentry-release=3.44.1.0,sentry-public_key=1f84b015401a455abc185c9ede2d18a1,sentry-trace_id=75efc58c2367446ab8d9d2750a17ef1c,sentry-sample_rate=0.2,sentry-transaction=%2FuserHome,sentry-sampled=false",
-              "content-type": "application/json",
-              priority: "u=1, i",
-              "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-              "sec-ch-ua-mobile": "?0",
-              "sec-ch-ua-platform": '"Windows"',
-              "sec-fetch-dest": "empty",
-              "sec-fetch-mode": "cors",
-              "sec-fetch-site": "same-origin",
-              "sec-gpc": "1",
-              "x-timeline-webversion": "3.44.1.0"
-            },
-            referrer: "https://linevoom.line.me/user/_dVkHnuS19n3F4QZRu6woElyJ1YUwrYkVVDKc2mk",
-            referrerPolicy: "strict-origin-when-cross-origin",
-            body: `{"postId":"${postId}"}`,
-            method: "POST",
-            mode: "cors",
-            credentials: "include"
-          });
         } else {
           console.log("with images");
         }
+        fetch("https://linevoom.line.me/api/post/delete?homeId=_dVkHnuS19n3F4QZRu6woElyJ1YUwrYkVVDKc2mk", {
+          headers: {
+            accept: "application/json, text/plain, */*",
+            "accept-language": "ja,en-US;q=0.9,en;q=0.8",
+            baggage: "sentry-environment=production,sentry-release=3.44.1.0,sentry-public_key=1f84b015401a455abc185c9ede2d18a1,sentry-trace_id=75efc58c2367446ab8d9d2750a17ef1c,sentry-sample_rate=0.2,sentry-transaction=%2FuserHome,sentry-sampled=false",
+            "content-type": "application/json",
+            priority: "u=1, i",
+            "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "sec-gpc": "1",
+            "x-timeline-webversion": "3.44.1.0"
+          },
+          referrer: "https://linevoom.line.me/user/_dVkHnuS19n3F4QZRu6woElyJ1YUwrYkVVDKc2mk",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          body: `{"postId":"${postId}"}`,
+          method: "POST",
+          mode: "cors",
+          credentials: "include"
+        });
         console.log(postId);
         console.log("\n");
         $$invalidate(0, ++nth);
